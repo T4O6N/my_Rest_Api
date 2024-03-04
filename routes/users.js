4e4077eab31683ad36610e2c9cbd90b4dfd1f27b
va1ent0n46 <3t9o9n9@gmail.com>
2024-03-04 14:39:29 +0700

    CM

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("../models/user");
 
 // Getting all
-router.get("/", async (req, res) => {
+router.get("/users", async (req, res) => {
   try {
     const users = await User.find();
     res.json(users);
