df1223341a2c1ea9da39d54120f7ae5df1fbf7b8
va1ent0n46 <3t9o9n9@gmail.com>
2024-03-04 11:07:31 +0700

    change mongodb connect

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("Connected to Database"));
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log({ message: err.message });
+  });
 
 app.use(express.json());
 
 const userRouter = require("./routes/users");
 app.use("/users", userRouter);
 
-app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is running at http://localhost:${PORT}`)
+);
